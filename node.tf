450e4f975256552611dccfcafffe21735f35a85e
pmawan <36118122+pmawan@users.noreply.github.com>
2020-09-24 19:11:48 +1000

    Create node.tf

---
+++ node.tf
@@ -0,0 +1,13 @@
+resource "bigip_ltm_node" "node" {
+  name             = "/Common/kibana_node"
+  address          = "1.1.1.1"
+  connection_limit = "0"
+  dynamic_ratio    = "1"
+  monitor          = "/Common/icmp"
+  description      = "kibana_server"
+  rate_limit       = "disabled"
+  fqdn {
+    address_family = "ipv4"
+    interval       = "3000"
+  }
+}
