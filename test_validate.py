78a85f333de3c06ab9ad9de3959664e9b5e5e97e
pmawan <36118122+pmawan@users.noreply.github.com>
2020-09-26 09:53:24 +1000

    Update test_validate.py

--- test_validate.py
+++ test_validate.py
@@ -1,8 +1,8 @@
 import requests
 requests.packages.urllib3.disable_warnings()
-x = requests.get('https://jsdgcsjugudcvdf.com', verify=False)
+x = requests.get('https://archiva.mpgsdev.mastercard.int/', verify=False)
 
-print("check - https://https://jsdgcsjugudcvdf.com")
+print("check - https://archiva.mpgsdev.mastercard.int/")
 print (x.status_code)
 if (x.status_code != 200):
   print("Fail")
