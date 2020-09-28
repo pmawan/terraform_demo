eacf53e389185a498d372aa60a631549b5a30d92
pmawan <36118122+pmawan@users.noreply.github.com>
2020-09-28 10:57:30 +1000

    Update kibana_pool.tf

--- kibana_pool.tf
+++ kibana_pool.tf
@@ -2,7 +2,7 @@ resource "bigip_ltm_pool" "pool" {
   name                = "/Common/kibana-pool"
   load_balancing_mode = "round-robin"
   description         = "Kibana Pool"
-  monitors            = [bigip_ltm_monitor.monitor.name, bigip_ltm_monitor.monitor2.name]
+  monitors            = [bigip_ltm_monitor.tcp_half_open]
   allow_snat          = "yes"
   allow_nat           = "yes"
 }
