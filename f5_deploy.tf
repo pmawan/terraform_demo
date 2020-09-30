ae5ea33082d83ce56190fc318f81494f707c00cd
pmawan <36118122+pmawan@users.noreply.github.com>
2020-09-30 11:50:19 +1000

    Create f5_deploy.tf

---
+++ f5_deploy.tf
@@ -0,0 +1,38 @@
+#create device ucs
+resource "bigip_command" "create-ucs" {
+  commands   = ["save sys ucs bip0syd0-pre-crq.ucs"]
+}
+
+
+resource "bigip_ltm_node" "node" {
+  name             = "/Common/kibana_node"
+  address          = "10.157.146.32"
+  connection_limit = "0"
+  dynamic_ratio    = "1"
+  monitor          = "/Common/icmp"
+}
+resource "bigip_ltm_pool" "pool" {
+  name                = "/Common/kibana-pool"
+  load_balancing_mode = "round-robin"
+  description         = "Kibana Pool"
+  monitors            = ["/Common/tcp_half_open"]
+  allow_snat          = "yes"
+  allow_nat           = "yes"
+}
+resource "bigip_ltm_pool_attachment" "attach_node" {
+  pool = "/Common/kibana-pool"
+  node = "/Common/kibana_node:5601"
+  depends_on = [bigip_ltm_pool.pool]
+}
+
+
+
+
+resource "bigip_ltm_virtual_server" "http" {
+        pool = "/Common/kibana-pool"
+        name = "/Common/kibana_vs_http"
+        destination = "10.1.10.100"
+        port = 80
+        source_address_translation = "automap"
+        depends_on = [bigip_ltm_pool.pool]
+}
