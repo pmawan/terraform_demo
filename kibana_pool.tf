3f02ff76018c909476997ea074a5e8400cc89765
pmawan <36118122+pmawan@users.noreply.github.com>
2020-09-30 11:49:10 +1000

    Delete kibana_pool.tf

--- kibana_pool.tf
+++
@@ -1,8 +0,0 @@
-resource "bigip_ltm_pool" "pool" {
-  name                = "/Common/kibana-pool"
-  load_balancing_mode = "round-robin"
-  description         = "Kibana Pool"
-  monitors            = [bigip_ltm_monitor.tcp_half_open]
-  allow_snat          = "yes"
-  allow_nat           = "yes"
-}
