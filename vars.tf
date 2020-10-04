b94a2093f1a0252e8f269e6e7ac758191b720a19
pmawan <36118122+pmawan@users.noreply.github.com>
2020-10-05 10:01:09 +1100

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,3 +1,23 @@
-variable "env" {
-        default = "stage"
+variable "env_name" {
+    type = string
+    default = "dev"
+}
+variable "vname" {
+    type = list
+    default = ["natp", "dine", "disc", "rbsi", "comp", "cmps", "npyt", "nglo"]
+}
+variable "vpconport" {
+    type = list
+    default = ["98765", "98764", "98763", "98762", "98761", "98766", "98767", "98768"]
+}
+variable "pname" {
+    type = list
+    default = ["p1", "p2"]
+}
+variable "pcon_tns_url" {
+    type = list
+    default = ["e31232.a.akamaiedge.net", "jenkins.io"]
+}
+variable "vip_ip" {
+    default = "10.10.10.20"
 }
